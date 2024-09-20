3880682c5c510f638c89b1806cb2cd5496ae5f4c
Igor Derevnin <shroom2207@yandex.ru>
2024-09-20 23:10:02 +0300

    Update Picture.py

--- Picture.py
+++ Picture.py
@@ -1,12 +1,19 @@
 from PIL import Image
 image=Image.open("image.jpg")
 red, green, blue = image.split()
-width_diff=100
-height_diff=50
-red_crop=red.crop((width_diff, height_diff, red.width, red.height))
-blue_crop=blue.crop((0, 0, blue.width-width_diff, blue.height-height_diff))
+width_diff=50
+height_diff=0
+
+red_left=red.crop((width_diff, height_diff, red.width, red.height))
+red_center=red.crop((width_diff//2, height_diff//2, red.width-width_diff//2, red.height-height_diff//2))
+red_shift=Image.blend(red_left, red_center, 0.2)
+
+blue_right=blue.crop((0, 0, blue.width-width_diff, blue.height-height_diff))
+blue_center=blue.crop((width_diff//2, height_diff//2, blue.width-width_diff//2, blue.height-height_diff//2))
+blue_shift=Image.blend(blue_right, blue_center, 0.2)
+
 green_crop=green.crop((width_diff//2, height_diff//2, green.width-width_diff//2, green.height-height_diff//2))
-image_merged=Image.merge("RGB", (red_crop, green_crop, blue_crop))
+image_merged=Image.merge("RGB", (red_shift, green_crop, blue_shift))
 image_merged.save("new_image.jpg")
 image_merged.thumbnail((80,80))
 image_merged.save("small_image.jpg")
